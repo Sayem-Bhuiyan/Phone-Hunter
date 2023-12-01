5fbf38f0ddbfa39d414ef133a4f18887fee54ff5
Sayem <sayem465508@gmail.com>
2023-12-01 21:08:32 +0600

    create phone hunter api

--- JS/phone.js
+++ JS/phone.js
@@ -1,23 +1,54 @@
-// function loadData(){
-//     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-// }
-// loadData()
+const loadPhone = async (search) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${search}`
+  )
+  const data = await res.json();
+  const phone = data.data;
+  // console.log(phone);
+  displayPhones(phone);
+};
 
-// const loadPhone =async () => {
-//     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-//     const data = await res.json()
-//     const phone = data.data;
-//     console.log(phone)
-// }
-// loadPhone()
+const displayPhones = (phones) => {
+//   console.log(phones)
+  const phoneContainer = document.getElementById('phones-container')
+  phoneContainer.textContent = ''
 
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    const data = await res.json()
-    const phone = data.data;
+  const showAllPhone = document.getElementById('show-all-container');
+  if(phones.length > 12){
+    showAllPhone.classList.remove('hidden');
+  }
+  else{
+    showAllPhone.classList.add('hidden')
+  }
+
+  phones = phones.slice(0,12)
+
+  phones.forEach((phone) => {
     console.log(phone);
+    // step-1: create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card p-4 bg-base-100 shadow-xl`;
+    phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Phone Image" /></figure>
+        <div class="flex flex-col justify-center items-center space-y-5">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p class="text-2xl font-medium">${phone.slug}</p>
+          <p class="text-3xl font-medium">$999</p>
+          <div class="card-actions justify-center">
+            <button class="btn btn-primary">Show Details</button>
+          </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard)
+  });
+};
+
+
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText)
+    loadPhone(searchText)
 }
 
-loadPhone()
+
