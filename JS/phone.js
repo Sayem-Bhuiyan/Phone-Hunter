123af79824738eca8bdebaa6b3c770a440862785
Sayem <sayem465508@gmail.com>
2023-12-02 08:36:08 +0600

    added loading spiner to phone hunter api

--- JS/phone.js
+++ JS/phone.js
@@ -41,14 +41,25 @@ const displayPhones = (phones) => {
         `;
         phoneContainer.appendChild(phoneCard)
   });
+  toggleLoadingSpinner(false)
 };
 
 
 const handleSearch = () => {
+    toggleLoadingSpinner(true)
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // console.log(searchText)
     loadPhone(searchText)
 }
 
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById('loading-spinner');
+  if(isLoading){
+    loadingSpinner.classList.remove('hidden');
+  }
+  else {
+    loadingSpinner.classList.add('hidden');
+  }
+}
 
